a595502bba205af2f85dd98f15869935c55e9b19
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-03-15 20:52:30 -0300

    refactor: change class name

--- src/app/repositories/userRepository/GetUserById/MysqlGetUserById.ts
+++ src/app/repositories/userRepository/GetUserById/MysqlGetUserById.ts
@@ -4,7 +4,9 @@ import User from '../../../entities/User';
 
 const prisma = new PrismaClient();
 
-export default class MysqlGetUserById implements IGetUserByIdRepository {
+export default class MysqlGetUserByIdRepository
+  implements IGetUserByIdRepository
+{
   async findById(userId: string): Promise<Omit<User, 'password'> | null> {
     const id = Number(userId);
     const user = await prisma.user.findUnique({
