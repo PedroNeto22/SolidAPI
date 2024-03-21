552bc700cd5a735a282090e2a2d75ef48237e8e5
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-03-21 07:38:34 -0300

    refactor: remove useless interface

--- src/app/repositories/userRepository/GetUserByEmail/IGetUserByEmailRepository.ts
+++ src/app/repositories/userRepository/GetUserByEmail/IGetUserByEmailRepository.ts
@@ -1,9 +1,5 @@
 import User from '../../../entities/User';
 
-interface UserData extends User {
-  id: string;
-}
-
 export interface IGetUserByEmailRepository {
-  findByEmail(email: string): Promise<Partial<UserData> | null>;
+  findByEmail(email: string): Promise<Partial<User> | null>;
 }
