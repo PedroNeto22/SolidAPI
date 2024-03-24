4aea350539b938d05f0c8b66e528e98a79202acf
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2024-03-24 18:19:56 -0300

    refactor: add promise

--- src/app/useCase/userUseCase/GetUserById/GetUserByIdUseCase.ts
+++ src/app/useCase/userUseCase/GetUserById/GetUserByIdUseCase.ts
@@ -5,8 +5,8 @@ import { IGetUserByIdRepository } from '../../../repositories/userRepository/Get
 export default class GetUserByIdUseCase implements IGetUserByIdUseCase {
   constructor(private readonly getUserByIdRepository: IGetUserByIdRepository) {}
 
-  execute(id: string): Promise<Partial<User> | null> {
-    const user = this.getUserByIdRepository.findById(id);
+  async execute(id: string): Promise<Partial<User> | null> {
+    const user = await this.getUserByIdRepository.findById(id);
 
     if (!user) {
       throw new Error('the user does not exist');
